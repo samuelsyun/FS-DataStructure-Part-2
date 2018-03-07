445617e70dd03ca4b1bbbc6dc379eb2dcf9ccb02
Samuel Yun <shysam.yun@gmail.com>
2018-03-07 11:00:46 -0500

    completed 03-02

--- 03_extra_credit/graph/02_findShortestPaths.js
+++ 03_extra_credit/graph/02_findShortestPaths.js
@@ -1,4 +1,29 @@
+'use strict'
 
-var shortestPaths = function(graph, start, end) {
-  //your code here
-}
+let shortestPaths = function (graph, current, end, path = []) {
+  path = path.concat(current);
+  let workingPathsArr = [];
+  let shortestPathsArr = [];
+
+  if (current === end) return [path];
+  else {
+    for (let neighbor of graph[current]){
+      if (path.indexOf(neighbor) === -1){
+        workingPathsArr = workingPathsArr.concat(shortestPaths(graph, neighbor, end, path));
+      }
+    }
+
+    let shortestlength = Infinity;
+
+    for ( let i = 0; i < workingPathsArr.length; i++){
+      if (workingPathsArr[i].length < shortestlength){
+        shortestlength = workingPathsArr[i].length;
+        shortestPathsArr = [workingPathsArr[i]];
+      }
+      else if (workingPathsArr[i].length === shortestlength){
+        shortestPathsArr.push(workingPathsArr[i]);
+      }
+    }
+    return shortestPathsArr;
+  }
+};
